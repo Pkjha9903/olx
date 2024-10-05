8081c5357e538dc0a7b48cb27f49dc7f1606c014
Pkjha9903 <160411541+Pkjha9903@users.noreply.github.com>
2024-10-05 12:57:30 +0530

    Update buyrequest.php

--- buyrequest.php
+++ buyrequest.php
@@ -2,11 +2,8 @@
 include 'dbcon.php';
 session_start();
 $itemid = (int)$_GET['itemid'];
-
 $r = $items->findOne(['itemid' => $itemid]);
-
 $sellerid = $r['sellerid'];
-
 $seller = $users->findOne(['userid' => $sellerid]);
 
 if (isset($_SESSION['userid'])) {
@@ -29,32 +26,25 @@ if (isset($_SESSION['userid'])) {
             $username = $_SESSION['username'];
             $item = $r['itemname'];
             $selleremail = $seller['useremail'];
-
             $to = $_SESSION['useremail'];
             $from = "From: toshitpant0808@gmail.com";
             $subject = "purchase request generated";
             $body = "hello $username your request for ( $item ) has been recorded. You can contact to seller. Contact details are as follows ; Email: $selleremail . We have also sent your mail id to the seller. In case of any problem, feel free to contact us. Our mail id and helpline numbers are mentioned in Contact Us page. Thank You !";
             mail($to, $subject, $body, $from);
-
             // send a mail to seller
             $sellername = $seller['username'];
             $buyeremail = $_SESSION['useremail'];
-
-
             $to2 = $seller['useremail'];
             $from2 = "From: toshitpant0808@gmail.com";
             $subject2 = "buy request";
             $body2 = "hello $sellername, someone is interested in buying your ( $item ). You can contact to buyer. Contact details are as follows ; Email: $buyeremail . We have also sent your mail id to the buyer. In case of any problem, feel free to contact us. Our mail id and helpline numbers are mentioned in Contact Us page. Thank You !";
             mail($to2, $subject2, $body2, $from2);
-
             // redirect to view more
             ?>
             <script>
                 location.replace('viewmore.php?itemid=<?php echo $itemid; ?>');
             </script>
         <?php
-
-
         } else {
         ?>
             <script>
