bdb1896c07340c25f9fe2cbcdb798d747c534465
Pkjha9903 <160411541+Pkjha9903@users.noreply.github.com>
2024-10-05 12:58:42 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -9,7 +9,6 @@ $questions = [];
 foreach ($questionsCursor as $document) {
   $questions[] = $document->question;
 }
-
 // Fetch all answers
 $answersCursor = $collection->find([], ['projection' => ['answer' => 1]]);
 $answers = [];
@@ -30,37 +29,31 @@ foreach ($answersCursor as $document) {
       background-color: #f8f9fa;
       color: #333;
     }
-
     .container {
       padding-top: 50px;
     }
-
     .about-section {
       text-align: center;
       padding-bottom: 50px;
       animation: slide-down 300ms ease-out forwards;
     }
-
     .section-title {
       font-size: 36px;
       font-weight: bold;
       margin-bottom: 30px;
       color: #E91E63;
     }
-
     #question-container {
       font-size: 18px;
       margin-bottom: 30px;
       color: #555;
     }
-
     .team-section {
       text-align: center;
       padding: 50px 0;
       background-color: #ff4c68;
       animation: meals-appear 1s ease-out forwards;
     }
-
     .team-member {
       margin-bottom: 30px;
       background-color: #fff;
@@ -74,25 +67,20 @@ foreach ($answersCursor as $document) {
       border-radius: 50%;
       box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
     }
-
     .team-member h3 {
       font-size: 24px;
       margin-top: 20px;
       color: #333;
     }
-
-
     .team-member {
       height: 100%;
       border: 5px solid rgba(0, 0, 0, 0.4);
     }
-
     @keyframes slide-down {
       from {
         opacity: 0;
         transform: translateY(-3rem)
       }
-
       to {
         opacity: 1;
         transform: translateY(0);
@@ -115,7 +103,6 @@ foreach ($answersCursor as $document) {
     function displayQuestions() {
       var questions = <?php echo json_encode($questions); ?>;
       var questionContainer = document.getElementById("question-container");
-
       // Display each question
       questions.forEach(function(question, index) {
         var questionElement = document.createElement("div");
@@ -126,13 +113,11 @@ foreach ($answersCursor as $document) {
         questionContainer.appendChild(questionElement);
       });
     }
-
     function displayAnswer(index) {
       var answers = <?php echo json_encode($answers); ?>;
       var answerContainer = document.getElementById("answer-container");
       answerContainer.innerHTML = answers[index];
       answerContainer.style.display = "block";
-
       // Hide answer after 15 seconds
       setTimeout(function() {
         answerContainer.style.display = "none";
@@ -140,7 +125,6 @@ foreach ($answersCursor as $document) {
     }
   </script>
 </head>
-
 <body onload="displayQuestions()">
   <!-- navbar start -->
   <?php include 'navbar.php'; ?>
@@ -153,7 +137,6 @@ foreach ($answersCursor as $document) {
     </div>
     <br />
     <br />
-
     <div class="team-section">
       <div class="row">
         <div class="col-md-12">
