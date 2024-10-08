d7525da254de6254fd096b02e4c68e41742cc343
Pkjha9903 <160411541+Pkjha9903@users.noreply.github.com>
2024-10-08 21:36:46 +0530

    Update tempo.php

--- tempo.php
+++ tempo.php
@@ -98,7 +98,6 @@
     </div>
   </div>
 
-  <!-- Bootstrap JS and dependencies -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
